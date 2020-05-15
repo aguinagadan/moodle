fdb17e5641f76d8db53c28216b2ccbb115b82f39
Samuel Antonio Rojas Pachas <samuelro444@gmail.com>
2020-05-15 13:24:39 -0500

    eliminar comentario innecesario

--- local/terminologia/version.php
+++ local/terminologia/version.php
@@ -17,7 +17,6 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->requires   = 2015111600;                       // This plugin requires Moodle VER 3.0.
-//$plugin->version    = 2019011107;                       // This plugins version number.
 $plugin->version    = 2019011107;                       // This plugins version number.
 $plugin->release    = 'v1.0';                           // This plugins release number.
 $plugin->maturity   = MATURITY_STABLE;
